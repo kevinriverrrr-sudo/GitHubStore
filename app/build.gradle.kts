5b622428eda14583aff42f653c7308822d0ba15d
Z User <z@container>
2026-05-19 11:39:25 +0000

    Disable minification for release build - full size APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,13 +29,9 @@ android {
             isMinifyEnabled = false
         }
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             signingConfig = signingConfigs.getByName("release")
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
 
